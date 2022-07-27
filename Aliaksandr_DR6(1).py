db81b73220692be937f002a325f453a576712e23
Aliaksandr Matusevich <109821192+matras2000@users.noreply.github.com>
2022-07-27 18:20:22 +0300

    homework

--- Aliaksandr_DR6(1).py
+++ Aliaksandr_DR6(1).py
@@ -1,2 +1,4 @@
-mylist = ['apple', 'banana', 'cherry']
-len(mylist)
+nums = [1, 5, 2, 1, 3, 5, 8]
+ # counter = [2, 2, 1, 2, 1, 2, 1]
+duplicate = [x for i, x in enumerate(nums) if i != nums.index(x)]
+print(duplicate)
